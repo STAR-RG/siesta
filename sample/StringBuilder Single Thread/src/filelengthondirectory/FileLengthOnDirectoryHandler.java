5bdf899958245e897cdd578b3e4fe19e70c9cd5f
Lucas Cabral <lucascostacabra@gmail.com>
2021-09-08 20:50:10 -0300

    fix squash into file length

--- sample/StringBuilder Single Thread/src/filelengthondirectory/FileLengthOnDirectoryHandler.java
+++ sample/StringBuilder Single Thread/src/filelengthondirectory/FileLengthOnDirectoryHandler.java
@@ -6,7 +6,7 @@ import org.aspectjml.lang.annotation.siesta.Before;
 public class FileLengthOnDirectoryHandler {
 
     // @Before("* java.io.File.length()")
-    @Before("java.io.File.length()")
+    @Before("public long java.io.File.length()")
     public static void vioOnCall_length_LengthOnDirectory(File file) {
         if(file.isDirectory())
             throw new RuntimeException("Vio: File length on dir.");
